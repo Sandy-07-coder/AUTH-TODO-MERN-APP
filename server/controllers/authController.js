8c5cba3a71472248aac6564084f4f3ba067d5589
Santhosh <santhoshprogrammer07@gmail.com>
2025-09-28 13:54:29 +0530

    updated secure to true

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,7 +16,7 @@ const registerNewUser = async (req, res) => {
 
         res.cookie("token",token,{
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: "None",
             maxAge: 24*60*60*1000
         });
